59c04b7bd1cffd127301c8c30cce07f8990147d6
DarkMGarner <markgarner334@gmail.com>
2023-03-21 22:29:44 -0500

    feat: add layout and auth page

---
+++ src/layouts/default.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <header>
+    ...
+  </header>
+  <main>
+    <router-view></router-view>
+  </main>
+  <footer>
+    ...
+  </footer>
+</template>
--- src/plugins/router.ts
+++ src/plugins/router.ts
@@ -3,5 +3,15 @@ import { createRouter, createWebHistory } from 'vue-router'
 export const router = createRouter({
   history: createWebHistory(),
   routes: [
-  ]
-})
+    {
+      path: '/',
+      component: () => import('@/layouts/default.vue'),
+      children: [
+        {
+          path: '',
+          component: () => import('@/pages/AuthPage.vue'),
+        },
+      ],
+    },
+  ],
+});
