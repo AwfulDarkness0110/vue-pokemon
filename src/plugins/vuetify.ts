fd16b90d7d86719081f040eec57d5c87743d881e
DarkMGarner <markgarner334@gmail.com>
2023-03-22 06:50:05 -0500

    refactor: use treeshaking config

--- src/plugins/vuetify.ts
+++ src/plugins/vuetify.ts
@@ -1,8 +1,5 @@
-import 'vuetify/styles'
 import '@mdi/font/css/materialdesignicons.css'
 import { createVuetify } from 'vuetify'
-import * as components from 'vuetify/components'
-import * as directives from 'vuetify/directives'
 import { aliases, mdi } from 'vuetify/iconsets/mdi'
 
 export const vuetify = createVuetify({
@@ -12,7 +9,5 @@ export const vuetify = createVuetify({
     sets: {
       mdi
     }
-  },
-  components,
-  directives
+  }
 })
