f9901be0fcdebdc5667f5bfc9c732832ca7980c0
DarkMGarner <markgarner334@gmail.com>
2023-03-22 19:31:18 -0500

    feat: add auth form

--- src/pages/AuthPage.vue
+++ src/pages/AuthPage.vue
@@ -1,5 +1,11 @@
+<script setup lang="ts">
+import AuthForm from '@/features/auth/AuthForm.vue'
+</script>
+
 <template>
   <section>
     <h1>Authentication</h1>
+
+    <auth-form></auth-form>
   </section>
 </template>
