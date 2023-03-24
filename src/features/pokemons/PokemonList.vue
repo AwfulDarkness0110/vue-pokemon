b59b4af057c6614bc2ecfa8ed0fa3985636b022a
DarkMGarner <markgarner334@gmail.com>
2023-03-23 23:13:21 -0500

    feat: add load more items

--- src/features/pokemons/PokemonList.vue
+++ src/features/pokemons/PokemonList.vue
@@ -3,9 +3,9 @@ import { useQuery } from '@vue/apollo-composable'
 import gql from 'graphql-tag'
 import { watch, computed } from 'vue'
 
-const { result, loading, error } = useQuery(gql`
-  query getPokemons {
-    pokemon_v2_pokemon(limit: 20, offset: 0) {
+const { result, loading, error, fetchMore } = useQuery(gql`
+  query getPokemons ($offset: Int, $limit: Int) {
+    pokemon_v2_pokemon(offset: $offset, limit: $limit) {
       id
       name
       weight
@@ -15,13 +15,31 @@ const { result, loading, error } = useQuery(gql`
         sprites
       }
     }
-  }`)
-
-const sprites = computed(() => result.value.pokemon_v2_pokemon.map((s) => ({
-  id: s.id,
-  sprites: JSON.parse(s.pokemon_v2_pokemonsprites[0].sprites)
+  }`, () => ({
+  offset: 0,
+  limit: 20
 }))
-)
+
+function loadMore() {
+  fetchMore({
+    variables: {
+      offset: result.value.pokemon_v2_pokemon.length
+    },
+    updateQuery: (previousResult, { fetchMoreResult }) => {
+      // No new feed posts
+      if (!fetchMoreResult) return previousResult
+
+      // Concat previous feed with new feed posts
+      return {
+        ...previousResult,
+        pokemon_v2_pokemon: [
+          ...previousResult.pokemon_v2_pokemon,
+          ...fetchMoreResult.pokemon_v2_pokemon
+        ]
+      }
+    }
+  })
+}
 </script>
 
 <template>
@@ -31,12 +49,14 @@ const sprites = computed(() => result.value.pokemon_v2_pokemon.map((s) => ({
     <v-row dense>
       <v-col cols="12" md="4" v-for="pokemon in result.pokemon_v2_pokemon" :key="pokemon.id">
         <v-card class="mx-auto">
-          <v-img class="align-end"
+          <v-img class="align-end" height="200"
             :src="`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${pokemon.id}.png`">
             <v-card-title v-text="pokemon.name"></v-card-title>
           </v-img>
         </v-card>
       </v-col>
     </v-row>
+
+    <v-btn class="mt-8" @click="loadMore">Load More</v-btn>
   </v-container>
 </template>
