39e7e0780a7fc2b23138ab95338b101a872f8123
DarkMGarner <markgarner334@gmail.com>
2023-03-25 09:35:42 -0500

    feat: add aria label

--- src/features/pokemons/components/ToggleFavorite.vue
+++ src/features/pokemons/components/ToggleFavorite.vue
@@ -6,8 +6,11 @@ const props = defineProps<{ pokemonId: number }>()
 const store = usePokemonStore()
 const { toggleFavorite } = store
 const buttonColor = computed(() => store.favorites.has(props.pokemonId) ? 'primary' : 'surface-variant')
+const actionText = computed(() => `${store.favorites.has(props.pokemonId) ? 'Remove' : 'Add'} Favorite`)
 </script>
 
 <template>
-  <v-btn @click="toggleFavorite(pokemonId)" size="small" :color="buttonColor" variant="text" icon="mdi-heart"></v-btn>
+  <v-btn @click="toggleFavorite(pokemonId)" :aria-label="actionText" size="small" :color="buttonColor" variant="text"
+    icon="mdi-heart">
+  </v-btn>
 </template>
